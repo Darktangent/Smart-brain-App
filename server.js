7b7438963d24407cb96c6bf4ac8762b4a6ff1b0c
rohan ganguly <rohan.ganguly@snhu.edu>
2018-08-11 10:02:18 -0500

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image =  require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-reticulated-65952',
     user : 'postgres',
     password : '',
     database : 'smart-brain'
